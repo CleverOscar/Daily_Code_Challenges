8bca3255477229a0e70a2574a727078076cd9427
Oscar Pacheco <Oscarpa88@yahoo.com>
2020-07-28 11:44:59 -0500

    Complete
    Two ways to solve Two_Strings

--- python/two_strings.py
+++ python/two_strings.py
@@ -3,3 +3,18 @@ Given two strings, determine if they share a common substring. A substring may b
 
 For example, the words "a", "and", "art" share the common substring . The words "be" and "cat" do not share a substring.
 '''
+
+def twoStrings(s1, s2):
+    # for char in s1:
+    #     if char in s2:
+    #         return "Yes"
+    #
+    # return 'No'
+
+    set1 = set(s1)
+    set2 = set(s2)
+
+    if len(set1 & set2) > 0:
+        return "YES"
+
+    return "NO"
