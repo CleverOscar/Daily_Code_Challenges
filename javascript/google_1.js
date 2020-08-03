0400fb0ec17669798e88dc9cb9444d708287b591
Oscar Pacheco <Oscarpa88@yahoo.com>
2020-08-03 16:03:44 -0500

    Update google_1.js

--- javascript/google_1.js
+++ javascript/google_1.js
@@ -2,16 +2,14 @@
 // return whether any two numbers from the list add up to k.
 // For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
-const integes = [10, 15, 3, 7]
+const numbers = [10, 15, 3, 7]
 
+
+// return true or false if the list
+// holds values that sum up to k
 function twoSums(list, k) {
-  // look for two integers
-  // iterate through list
-  // for int of list
-  //  if int1 + int2 == k {
-  //    return true
-  //    } else {
-  //    return false
-  //    }
+  // store numbers to add up
+  // iterate over list to see
+  // if it contains values 
   //
 }
