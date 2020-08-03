62d22d9d9fea95e4d3584a46bd4b442191f01366
Oscar Pacheco <Oscarpa88@yahoo.com>
2020-08-03 11:24:20 -0500

    google code challenge

---
+++ javascript/google_1.js
@@ -0,0 +1,17 @@
+// Given a list of numbers and a number k,
+// return whether any two numbers from the list add up to k.
+// For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+const integes = [10, 15, 3, 7]
+
+function twoSums(list, k) {
+  // look for two integers
+  // iterate through list
+  // for int of list
+  //  if int1 + int2 == k {
+  //    return true
+  //    } else {
+  //    return false
+  //    }
+  //
+}
