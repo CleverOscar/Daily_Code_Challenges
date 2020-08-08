5bb75709c44e68d569abfb4f4e4f7c1fa6881843
Oscar Pacheco <Oscarpa88@yahoo.com>
2020-08-08 16:27:03 -0500

    update
    Adding to the documents file

--- javascript/Tetris/readMe.md
+++ javascript/Tetris/readMe.md
@@ -3,3 +3,6 @@
 #### Basic First Steps
 
 create the index.html file needed to render out our canvas element and have our tetris.js file.
+
+
+> canvas variable 
--- javascript/Tetris/tetris.js
+++ javascript/Tetris/tetris.js
@@ -9,6 +9,14 @@ const matrix = [
   [0,1,0],
 ]
 
+function collide(arena, player) {
+  const [m, o] = [player.matrix, player.pos];
+
+  for (let y = 0; y < matrix.length; ++y) {
+    for (let x = 0; x < m[y].length; ++x)
+  }
+}
+
 function drawMatrix(matrix, offset) {
   matrix.forEach((row, y) => {
     row.forEach((value, x) => {
@@ -23,6 +31,25 @@ function drawMatrix(matrix, offset) {
   })
 }
 
+function createMatrix(width, height) {
+  const matrix = [];
+
+  while (height --) {
+    matrix.push(new Array(width).fill(0))
+  }
+
+  return matrix;
+}
+
+function merge(arena, player){
+  player.matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        arena[y + player.pos.y][x + player.pos.x] = value
+      }
+    })
+  })
+}
 
 function draw() {
   context.fillStyle = '#000';
@@ -35,6 +62,11 @@ let lastTime = 0;
 let dropCounter = 0;
 let dropInterval = 1000;
 
+function playerDrop() {
+  player.pos.y++;
+  dropCounter = 0;
+}
+
 function update(time = 0) {
   const deltaTime = time - lastTime
   lastTime = time
@@ -42,13 +74,15 @@ function update(time = 0) {
   dropCounter += deltaTime;
 
   if (dropCounter > dropInterval) {
-    player.pos.y++;
-    dropCounter = 0;
+    playerDrop();
   }
 
   draw();
   requestAnimationFrame(update)
 }
+
+const arena = createMatrix(12, 20);
+
 
 const player = {
   pos: {x: 5, y: 5},
@@ -61,8 +95,7 @@ document.addEventListener('keydown', event => {
   } else if (event.keyCode === 39 ) {
     player.pos.x++
   } else if(event.keyCode === 40) {
-    player.pos.y++
-    dropCounter = 0;
+    playerDrop();
   }
 })
 
