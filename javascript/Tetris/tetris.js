6c8823db70caf3740bd29915f278630559a83a1b
Oscar Pacheco <Oscarpa88@yahoo.com>
2020-08-11 14:32:30 -0500

    More Functionality

--- javascript/Tetris/tetris.js
+++ javascript/Tetris/tetris.js
@@ -9,11 +9,6 @@ const context = canvas.getContext("2d")
 context.scale(20,20)
 
 // tetris matrix , T shape hardcoded in for now
-const tetris = [
-  [0,0,0],
-  [1,1,1],
-  [0,1,0],
-];
 
 function collide(arena, player) {
   const [m, o] = [player.tetris, player.pos];
@@ -39,13 +34,56 @@ function createTetris(w, h){
   return matrix
 }
 
+function createPiece(type) {
+  if (type === 'T') {
+    return [
+      [0,0,0],
+      [1,1,1],
+      [0,1,0],
+    ];
+  }
+}
+
+function rotate(matrix, dir) {
+  for (let y = 0; y < matrix.length; ++y){
+    for (let x = 0; x < y; ++x) {
+      [
+        matrix[x][y],
+        matrix[y][x],
+      ] = [
+        matrix[y][x],
+        matrix[x][y],
+      ];
+    }
+  }
+  if (dir > 0) {
+    matrix.forEach(row => row.reverse());
+  } else {
+    matrix.reverse();
+  }
+}
+
+function playerRotate(dir) {
+  const pos = player.pos.x;
+  let offset = 1;
+  rotate(player.tetris, dir);
+  while (collide(arena, player)) {
+    player.pos.x += offset;
+    offset = -(offset + (offset > 0 ? 1 : -1));
+    if (offset > player.tetris[0].length) {
+      rotate(player.tetris -dir);
+      player.pos.x = pos;
+      return;
+    }
+  }
+}
 
 function draw() {
   // fillStyle property can be any css color
   context.fillStyle = '#000'
   // fillRect(x,y, width, height)
   context.fillRect(0,0, canvas.width, canvas.height);
-  drawTetris(arena, {x: 0, y:0})
+  drawTetris(arena, {x: 0, y:0 })
   drawTetris(player.tetris, player.pos);
 }
 
@@ -77,6 +115,13 @@ function playerDrop(){
   dropCounter = 0;
 }
 
+function playerMove(dir) {
+  player.pos.x += dir;
+  if(collide(arena,player)) {
+    player.pos.x -= dir;
+  }
+}
+
 function update(time = 0){
   const deltaTime = time - lastTime;
   lastTime = time
@@ -102,7 +147,7 @@ function merge(arena, player) {
 
 const player = {
   pos: {x: 5, y: 5},
-  tetris: tetris
+  tetris: createPiece('T')
 }
 
 const arena = createTetris(12,20);
@@ -111,11 +156,17 @@ console.table(arena);
 
 document.addEventListener('keydown', e => {
   if (e.keyCode === 37){
-    player.pos.x--;
+    playerMove(-1)
   } else if (e.keyCode === 39){
-    player.pos.x++;
+    playerMove(+1)
   } else if (e.keyCode === 40){
     playerDrop();
+  } else if (e.keyCode === 82) {
+    playerRotate(-1)
+  } else if (e.keyCode === 82) {
+    playerRotate(-1)
+  } else if (e.keyCode === 84) {
+    playerRotate(1)
   }
 })
 
